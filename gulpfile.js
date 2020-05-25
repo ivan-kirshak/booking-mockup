a71b15e9fd905692406b92fa7d30f8b3f64b630e
Ivan Kirshak <i.kirshak@gmail.com>
2020-05-25 15:58:12 +0300

    figuring out autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+// const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 let uglify = require('gulp-uglify-es').default;
 
 // SCSS compile (Gulp 4 syntax)
-sass.compiler = require('node-sass');
-function style() {
-    return gulp.src('./src/css/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./build/css'))
-}
+// sass.compiler = require('node-sass');
+// function style() {
+//     return gulp.src('./src/css/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./build/css'))
+// }
 
 // copy HTML
-function html () {
+function html() {
     return gulp.src("src/*.html")
         .pipe(gulp.dest('build'));
 }
 
-function images () {
+function images() {
     return gulp.src('src/media/*')
         .pipe(imagemin())
         .pipe(gulp.dest('build/media'))
@@ -29,7 +29,21 @@ gulp.task("uglify", function () {
         .pipe(gulp.dest("build/js"));
 });
 
+
+// just trying to use autoprefixes with clean css
+gulp.task('autoprefixer', () => {
+    const autoprefixer = require('autoprefixer')
+    const sourcemaps = require('gulp-sourcemaps')
+    const postcss = require('gulp-postcss')
+   
+    return gulp.src('./src/*.css')
+      .pipe(sourcemaps.init())
+      .pipe(postcss([ autoprefixer() ]))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./build/css'))
+  })
+
 //line below makes function above run when called
-exports.style = style;
+// exports.style = style;
 exports.html = html;
 exports.images = images;
